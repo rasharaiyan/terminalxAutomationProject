e4713eaf613a7d269058b58c1a5bda13013ae38b
Rasha <rasharaiyan00@gmail.com>
2024-03-25 12:36:25 +0200

    update tests_cart_functionality_api.py

--- tests/api_tests/tests_cart_functionality_api.py
+++ tests/api_tests/tests_cart_functionality_api.py
@@ -1,7 +1,6 @@
 from infra.api_test_base import APITestBase
 from logic.api_page import UserPage
 
-
 class CartFunctionality(APITestBase):
 
     def setUp(self):
@@ -14,34 +13,31 @@ class CartFunctionality(APITestBase):
         self.sku = self.config['Tests']['product']['sku']
         self.qty_update = self.config['Tests']['product']['qty_update']
 
-
     def test_add_item_to_cart(self):
         response, private_content_version_cookie, counter_cookie, PHPSESSID_cookie = UserPage.login(self.base_url, self.email, self.password)
-
-        response = UserPage.get_user_cart_info(self.base_url, private_content_version_cookie, counter_cookie,
-                                               PHPSESSID_cookie)
+        response = UserPage.get_user_cart_info(self.base_url, private_content_version_cookie, counter_cookie, PHPSESSID_cookie)
         if response['data']['currentUserInfo']['cart_object']['items']:
             item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
-
-            response = UserPage.remove_item_from_cart(self.base_url, private_content_version_cookie, counter_cookie,
-                                                      PHPSESSID_cookie, item_id)
-
+            response = UserPage.remove_item_from_cart(self.base_url, private_content_version_cookie, counter_cookie, PHPSESSID_cookie, item_id)
         response = UserPage.add_to_cart(self.base_url, self.qty, self.sku, private_content_version_cookie, counter_cookie, PHPSESSID_cookie)
         assert response['data']['addAnyProductsToAnyCart']['total_quantity'] == self.qty
 
     def test_update_item_in_cart(self):
         response, private_content_version_cookie, counter_cookie, PHPSESSID_cookie = UserPage.login(self.base_url, self.email, self.password)
         response = UserPage.get_user_cart_info(self.base_url, private_content_version_cookie, counter_cookie, PHPSESSID_cookie)
-        item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
-        print(item_id)
-        response = UserPage.update_item_in_cart(self.base_url, self.qty_update, private_content_version_cookie, counter_cookie, PHPSESSID_cookie, item_id)
-        assert response['data']['updateAnyCartItems']['items'][0]['quantity'] == self.qty_update
+        if response['data']['currentUserInfo']['cart_object']['items']:
+            item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
+            response = UserPage.update_item_in_cart(self.base_url, self.qty_update, private_content_version_cookie, counter_cookie, PHPSESSID_cookie, item_id)
+            assert response['data']['updateAnyCartItems']['items'][0]['quantity'] == self.qty_update
+        else:
+            self.fail("No items in cart to update.")
 
     def test_remove_item_from_cart(self):
         response, private_content_version_cookie, counter_cookie, PHPSESSID_cookie = UserPage.login(self.base_url, self.email, self.password)
         response = UserPage.get_user_cart_info(self.base_url, private_content_version_cookie, counter_cookie, PHPSESSID_cookie)
-        item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
-        response = UserPage.remove_item_from_cart(self.base_url, private_content_version_cookie, counter_cookie, PHPSESSID_cookie, item_id)
-        assert response['data']['removeItemFromAnyCart']['total_quantity'] == 0
-
-
+        if response['data']['currentUserInfo']['cart_object']['items']:
+            item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
+            response = UserPage.remove_item_from_cart(self.base_url, private_content_version_cookie, counter_cookie, PHPSESSID_cookie, item_id)
+            assert response['data']['removeItemFromAnyCart']['total_quantity'] == 0
+        else:
+            self.fail("No items in cart to remove.")
