6fa57ec423b17f34a5e4f1dc6baaedbc5b5f75f4
Rasha <rasharaiyan00@gmail.com>
2024-03-25 13:46:50 +0200

    update tests_cart_functionality_api.py

--- tests/api_tests/tests_cart_functionality_api.py
+++ tests/api_tests/tests_cart_functionality_api.py
@@ -13,7 +13,28 @@ class CartFunctionality(APITestBase):
         self.sku = self.config['Tests']['product']['sku']
         self.qty_update = self.config['Tests']['product']['qty_update']
 
+    def clear_cart(self):
+        # Log in to get the necessary cookies and session information
+        response, private_content_version_cookie, counter_cookie, PHPSESSID_cookie = UserPage.login(self.base_url,
+                                                                                                    self.email,
+                                                                                                    self.password)
+        assert response['data']['userLogin']['customer_id'] == self.customer_id
+
+        # Get current cart info
+        response = UserPage.get_user_cart_info(self.base_url, private_content_version_cookie, counter_cookie,
+                                               PHPSESSID_cookie)
+        items = response['data']['currentUserInfo']['cart_object']['items']
+
+        # Remove all items from the cart
+        for item in items:
+            item_id = item['id']
+            response = UserPage.remove_item_from_cart(self.base_url, private_content_version_cookie, counter_cookie,
+                                                      PHPSESSID_cookie, item_id)
+            assert response['data']['removeItemFromAnyCart'][
+                       'total_quantity'] >= 0  # Check this condition based on how your API behaves
+
     def test_add_item_to_cart(self):
+        self.clear_cart()
         response, private_content_version_cookie, counter_cookie, PHPSESSID_cookie = UserPage.login(self.base_url, self.email,  self.password)
 
         assert response['data']['userLogin']['customer_id'] == self.customer_id
