c06ad49753ead8216d311eb8443f2e426e93b8da
Rasha <rasharaiyan00@gmail.com>
2024-03-25 10:28:04 +0200

    update tests_cart_functionality_api.py

--- tests/api_tests/api_test_runner.py
+++ tests/api_tests/api_test_runner.py
@@ -1,24 +1,24 @@
 import unittest
-from tests.api_tests.test_cart_functionality import CartFunctionality
+from tests.api_tests.tests_cart_functionality_api import CartFunctionality
 from tests.ui_tests.test_verify_added_product_to_cart import TestVerifyAddedProductToCart
 from tests.ui_tests.test_verify_updated_product_qty_in_cart import TestVerifyUpdatedProductQtyInCart
 from tests.ui_tests.test_verify_deleted_product_from_cart import TestVerifyDeletedProductFromCart
 
 # Initialize test loader
 loader = unittest.TestLoader()
-api_add_to_cart = loader.loadTestsFromName('tests.api_tests.test_cart_functionality.CartFunctionality.test_add_item_to_cart')
+api_add_to_cart = loader.loadTestsFromName('tests.api_tests.tests_cart_functionality_api.CartFunctionality.test_add_item_to_cart')
 add_to_cart_test = loader.loadTestsFromTestCase(TestVerifyAddedProductToCart)
 test_suite = unittest.TestSuite([api_add_to_cart])
 test_runner = unittest.TextTestRunner()
 test_runner.run(test_suite)
 
-api_update_item_quantity_in_cart = loader.loadTestsFromName('tests.api_tests.test_cart_functionality.CartFunctionality.test_update_item_in_cart')
+api_update_item_quantity_in_cart = loader.loadTestsFromName('tests.api_tests.tests_cart_functionality_api.CartFunctionality.test_update_item_in_cart')
 update_qty_in_cart_test = loader.loadTestsFromTestCase(TestVerifyUpdatedProductQtyInCart)
 test_suite = unittest.TestSuite([api_update_item_quantity_in_cart])
 test_runner = unittest.TextTestRunner()
 test_runner.run(test_suite)
 
-api_remove_from_cart = loader.loadTestsFromName('tests.api_tests.test_cart_functionality.CartFunctionality.test_remove_item_from_cart')
+api_remove_from_cart = loader.loadTestsFromName('tests.api_tests.tests_cart_functionality_api.CartFunctionality.test_remove_item_from_cart')
 delete_product_from_cart_test = loader.loadTestsFromTestCase(TestVerifyDeletedProductFromCart)
 test_suite = unittest.TestSuite([api_remove_from_cart])
 test_runner = unittest.TextTestRunner()
