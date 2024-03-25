61935910d22461f91ad1e15ca0d852097122b622
Rasha <rasharaiyan00@gmail.com>
2024-03-25 11:24:14 +0200

    update tests_cart_functionality_api.py

--- tests/api_tests/tests_cart_functionality_api.py
+++ tests/api_tests/tests_cart_functionality_api.py
@@ -32,15 +32,15 @@ class CartFunctionality(APITestBase):
     def test_update_item_in_cart(self):
         response, private_content_version_cookie, counter_cookie, PHPSESSID_cookie = UserPage.login(self.base_url, self.email, self.password)
         response = UserPage.get_user_cart_info(self.base_url, private_content_version_cookie, counter_cookie, PHPSESSID_cookie)
-        # item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
-        # print(item_id)
+        item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
+        print(item_id)
         response = UserPage.update_item_in_cart(self.base_url, self.qty_update, private_content_version_cookie, counter_cookie, PHPSESSID_cookie, item_id)
         assert response['data']['updateAnyCartItems']['items'][0]['quantity'] == self.qty_update
 
     def test_remove_item_from_cart(self):
         response, private_content_version_cookie, counter_cookie, PHPSESSID_cookie = UserPage.login(self.base_url, self.email, self.password)
         response = UserPage.get_user_cart_info(self.base_url, private_content_version_cookie, counter_cookie, PHPSESSID_cookie)
-        #item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
+        item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
         response = UserPage.remove_item_from_cart(self.base_url, private_content_version_cookie, counter_cookie, PHPSESSID_cookie, item_id)
         assert response['data']['removeItemFromAnyCart']['total_quantity'] == 0
 
