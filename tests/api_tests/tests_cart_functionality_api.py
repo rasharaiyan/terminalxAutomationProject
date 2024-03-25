8182ab165f31682f7c882d5da5617233c0b4021a
Rasha <rasharaiyan00@gmail.com>
2024-03-25 12:49:43 +0200

    update tests_cart_functionality_api.py

--- tests/api_tests/tests_cart_functionality_api.py
+++ tests/api_tests/tests_cart_functionality_api.py
@@ -14,30 +14,51 @@ class CartFunctionality(APITestBase):
         self.qty_update = self.config['Tests']['product']['qty_update']
 
     def test_add_item_to_cart(self):
-        response, private_content_version_cookie, counter_cookie, PHPSESSID_cookie = UserPage.login(self.base_url, self.email, self.password)
-        response = UserPage.get_user_cart_info(self.base_url, private_content_version_cookie, counter_cookie, PHPSESSID_cookie)
+        response, private_content_version_cookie, counter_cookie, PHPSESSID_cookie = UserPage.login(self.base_url,
+                                                                                                    self.email,
+                                                                                                    self.password)
+
+        response = UserPage.get_user_cart_info(self.base_url, private_content_version_cookie, counter_cookie,
+                                               PHPSESSID_cookie)
         if response['data']['currentUserInfo']['cart_object']['items']:
             item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
-            response = UserPage.remove_item_from_cart(self.base_url, private_content_version_cookie, counter_cookie, PHPSESSID_cookie, item_id)
-        response = UserPage.add_to_cart(self.base_url, self.qty, self.sku, private_content_version_cookie, counter_cookie, PHPSESSID_cookie)
+
+            response = UserPage.remove_item_from_cart(self.base_url, private_content_version_cookie, counter_cookie,
+                                                      PHPSESSID_cookie, item_id)
+
+        response = UserPage.add_to_cart(self.base_url, self.qty, self.sku, private_content_version_cookie,
+                                        counter_cookie, PHPSESSID_cookie)
         assert response['data']['addAnyProductsToAnyCart']['total_quantity'] == self.qty
 
     def test_update_item_in_cart(self):
-        response, private_content_version_cookie, counter_cookie, PHPSESSID_cookie = UserPage.login(self.base_url, self.email, self.password)
-        response = UserPage.get_user_cart_info(self.base_url, private_content_version_cookie, counter_cookie, PHPSESSID_cookie)
-        if response['data']['currentUserInfo']['cart_object']['items']:
-            item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
-            response = UserPage.update_item_in_cart(self.base_url, self.qty_update, private_content_version_cookie, counter_cookie, PHPSESSID_cookie, item_id)
-            assert response['data']['updateAnyCartItems']['items'][0]['quantity'] == self.qty_update
-        else:
-            self.fail("No items in cart to update.")
+        response, private_content_version_cookie, counter_cookie, PHPSESSID_cookie = UserPage.login(self.base_url,
+                                                                                                    self.email,
+                                                                                                    self.password)
+        assert response['data']['userLogin']['customer_id'] == self.customer_id
+
+        response = UserPage.get_user_cart_info(self.base_url, private_content_version_cookie, counter_cookie,
+                                               PHPSESSID_cookie)
+        assert response['data']['currentUserInfo']['cart_object']['items'][0]['id'] is not None
+
+        item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
+        print(item_id)
+
+        response = UserPage.update_item_in_cart(self.base_url, self.qty_update, private_content_version_cookie,
+                                                counter_cookie, PHPSESSID_cookie, item_id)
+        assert response['data']['updateAnyCartItems']['items'][0]['quantity'] == self.qty_update
 
     def test_remove_item_from_cart(self):
-        response, private_content_version_cookie, counter_cookie, PHPSESSID_cookie = UserPage.login(self.base_url, self.email, self.password)
-        response = UserPage.get_user_cart_info(self.base_url, private_content_version_cookie, counter_cookie, PHPSESSID_cookie)
-        if response['data']['currentUserInfo']['cart_object']['items']:
-            item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
-            response = UserPage.remove_item_from_cart(self.base_url, private_content_version_cookie, counter_cookie, PHPSESSID_cookie, item_id)
-            assert response['data']['removeItemFromAnyCart']['total_quantity'] == 0
-        else:
-            self.fail("No items in cart to remove.")
+        response, private_content_version_cookie, counter_cookie, PHPSESSID_cookie = UserPage.login(self.base_url,
+                                                                                                    self.email,
+                                                                                                    self.password)
+        assert response['data']['userLogin']['customer_id'] == self.customer_id
+
+        response = UserPage.get_user_cart_info(self.base_url, private_content_version_cookie, counter_cookie,
+                                               PHPSESSID_cookie)
+        assert response['data']['currentUserInfo']['cart_object']['items'][0]['id'] is not None
+
+        item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
+
+        response = UserPage.remove_item_from_cart(self.base_url, private_content_version_cookie, counter_cookie,
+                                                  PHPSESSID_cookie, item_id)
+        assert response['data']['removeItemFromAnyCart']['total_quantity'] == 0
