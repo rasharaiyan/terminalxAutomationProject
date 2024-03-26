de5fbca9ff628afff4a41c02d12e91ee0de13b58
Rasha <rasharaiyan00@gmail.com>
2024-03-26 10:30:14 +0200

    update api_utils.py

--- infra/api_utils.py
+++ infra/api_utils.py
@@ -41,7 +41,7 @@ class APIUtils:
         else:
             print("POST request failed with status code:", response.status_code)
 
-        #print("Response content:", response.text)
+        print("Response content:", response.text)
         return response.json()
 
     @staticmethod
@@ -61,7 +61,7 @@ class APIUtils:
         else:
             print("POST request failed with status code:", response.status_code)
 
-        #print("Response content:", response.text)
+        print("Response content:", response.text)
         return response.json()
 
     @staticmethod
@@ -82,7 +82,7 @@ class APIUtils:
         else:
             print("POST request failed with status code:", response.status_code)
 
-        #print("Response content:", response.text)
+        print("Response content:", response.text)
         return response.json()
 
     @staticmethod
