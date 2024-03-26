5c4311d4ab6a317bf3b8a0398859f83e54682e3d
Rasha <rasharaiyan00@gmail.com>
2024-03-26 09:34:17 +0200

    add test_api_update_item_in_cart_functionality.py

---
+++ tests/api_tests/test_api_update_item_in_cart_functionality.py
@@ -0,0 +1,32 @@
+from infra.api_test_base import APITestBase
+from logic.api_page import UserPage
+
+
+class UpdateInCartThroughAPI(APITestBase):
+
+    def setUp(self):
+        self.base_url = self.config['Tests']['baseURL']
+        self.email = self.config['Tests']['loginCredentials']['email']
+        self.password = self.config['Tests']['loginCredentials']['password']
+        self.expected_user_name = self.config['Tests']['loginCredentials']['expected_user_name']
+        self.customer_id = self.config['Tests']['customerInfo']['customer_id']
+        self.qty = self.config['Tests']['product']['qty']
+        self.sku = self.config['Tests']['product']['sku']
+        self.qty_update = self.config['Tests']['product']['qty_update']
+
+    def test_update_item_in_cart(self):
+        response, private_content_version_cookie, counter_cookie, PHPSESSID_cookie = UserPage.login(self.base_url,
+                                                                                                    self.email,
+                                                                                                    self.password)
+        assert response['data']['userLogin']['customer_id'] == self.customer_id
+
+        response = UserPage.get_user_cart_info(self.base_url, private_content_version_cookie, counter_cookie,
+                                               PHPSESSID_cookie)
+        assert response['data']['currentUserInfo']['cart_object']['items'][0]['id'] is not None
+
+        item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
+        print(item_id)
+
+        response = UserPage.update_item_in_cart(self.base_url, self.qty_update, private_content_version_cookie,
+                                                counter_cookie, PHPSESSID_cookie, item_id)
+        assert response['data']['updateAnyCartItems']['items'][0]['quantity'] == self.qty_update
