fb6704d7b095e31aaaec8a33197416a73e46f86a
Rasha <rasharaiyan00@gmail.com>
2024-03-27 15:09:19 +0200

    update api_ui_test_runner.py

--- tests/api_ui_test_runner.py
+++ tests/api_ui_test_runner.py
@@ -1,7 +1,7 @@
 import unittest
 from api_tests.test_api_add_item_to_cart_functionality import AddToCartThroughAPI
 from ui_tests.test_verify_added_product_to_cart import TestVerifyAddedProductToCart
-
+from ui_tests.test_price_sorting import TestPriceSorting
 # Initialize test loader
 loader = unittest.TestLoader()
 
@@ -9,4 +9,8 @@ api_add_to_cart = loader.loadTestsFromTestCase(AddToCartThroughAPI)
 add_to_cart_test = loader.loadTestsFromTestCase(TestVerifyAddedProductToCart)
 test_suite = unittest.TestSuite([api_add_to_cart, add_to_cart_test])
 test_runner = unittest.TextTestRunner()
+test_runner.run(test_suite)
+
+price_sorting = loader.loadTestsFromTestCase(TestPriceSorting)
+test_suite = unittest.TestSuite([price_sorting])
 test_runner.run(test_suite)
