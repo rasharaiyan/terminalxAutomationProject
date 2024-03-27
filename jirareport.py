830e68775cc45d866a83663190ea61805308aa98
Rasha <rasharaiyan00@gmail.com>
2024-03-27 15:06:16 +0200

    UPDATE test_price_sorting.py with the bug creation in jira

--- jirareport.py
+++ jirareport.py
@@ -1,6 +1,5 @@
 from jira import JIRA
-
-
+import traceback
 
 class JiraReport:
 
@@ -12,11 +11,16 @@ class JiraReport:
         self.auth_jira = JIRA(basic_auth=(self.jira_email, self.jira_token), options={'server': self.jira_url})
 
     def create_issue(self, summery, description, issue_type="Bug"):
-        issue_dict = {
-            'project': {'key': self.jira_project_key},
-            'summary': f'failed test: {summery}',
-            'description': description,
-            'issuetype': {'name': issue_type},
-        }
-        new_issue = self.auth_jira.create_issue(fields=issue_dict)
-        return new_issue.key
+        try:
+            issue_dict = {
+                'project': {'key': self.jira_project_key},
+                'summary': f'failed test: {summery}',
+                'description': description,
+                'issuetype': {'name': issue_type},
+            }
+            new_issue = self.auth_jira.create_issue(fields=issue_dict)
+            return new_issue.key
+        except Exception as e:
+            print("An error occurred while creating the Jira issue:")
+            print(traceback.format_exc())
+            return None
