bd94f4dbacc729da08a92ed6c243b3575388e905
Rasha <rasharaiyan00@gmail.com>
2024-03-27 19:10:37 +0200

    update jirareport.py to take the token from .env file

--- jirareport.py
+++ jirareport.py
@@ -4,7 +4,8 @@ import traceback
 class JiraReport:
 
     def __init__(self):
-        self.jira_token = "ATATT3xFfGF0fWzYpVgg6i8DGmIquIZ1WwLq4vitLumEEJRGXkeckjoe4yuh1DpXgd8CBev6a2T_hfq6LkROFeZGyq43C4bOhUSM033U9z1ORjHlnOOPqBQfsrKdj31BUOsLcp58cTW-QDd2JKPvrAqIqO2dckMed9vwTYIomKMHfMIHa1TvYIk=2F847502"
+        self.getenv = GetENV()
+        self.jira_token = self.getenv.get_jira_token()
         self.jira_url = "https://rasharaiyanbd24.atlassian.net/"
         self.jira_email = "rasharaiyan00@gmail.com"
         self.jira_project_key = "KAN"
