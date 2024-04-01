e32c70657a59f61fba135d43e5b2b46fe9785cd1
Rasha <rasharaiyan00@gmail.com>
2024-04-02 01:35:36 +0300

    modify  test_api_update_item_in_cart_functionality.py to take the createissue method from api_tes_base

--- tests/api_tests/test_api_update_item_in_cart_functionality.py
+++ tests/api_tests/test_api_update_item_in_cart_functionality.py
@@ -5,6 +5,8 @@ from logic.api_page import UserPage
 class UpdateInCartThroughAPI(APITestBase):
 
     def setUp(self):
+        super().setUp()
+        self.test_passed = True  # Initialize test_passed flag as True
         self.base_url = self.config['Tests']['baseURL']
         self.email = self.config['Tests']['loginCredentials']['email']
         self.password = self.config['Tests']['loginCredentials']['password']
@@ -25,11 +27,19 @@ class UpdateInCartThroughAPI(APITestBase):
         # Check if the cart is not empty before attempting to access its first item
         if response['data']['currentUserInfo']['cart_object']['items']:
             item_id = response['data']['currentUserInfo']['cart_object']['items'][0]['id']
-            print(item_id)
 
             # Only attempt to update the cart if it is not empty
             response = UserPage.update_item_in_cart(self.base_url, self.qty_update, private_content_version_cookie,
                                                     counter_cookie, PHPSESSID_cookie, item_id)
             assert response['data']['updateAnyCartItems']['items'][0]['quantity'] == self.qty_update
+            self.test_passed = True
         else:
             self.fail("The cart is empty. Expected at least one item in the cart.")
+            self.test_passed = False
+
+    def tearDown(self):
+        super().tearDown()  # Call the tearDown method of the parent class
+        if not self.test_passed:
+            # Assertion failed, create Jira issue using the method from TestBase
+            self.report_jira_issue("Update Item In Cart Through API Test Assertion Failure",
+                                   "Test failed due to assertion failure")
